32608432753dac8fa699a47be0f6aa1518e2dba7
Michal-Atlas <31102317+Michal-Atlas@users.noreply.github.com>
2022-02-12 23:19:54 +0100

    CI: FINAL

--- publishing.el
+++ publishing.el
@@ -1,10 +1,11 @@
 (require 'org)
-(require 'ox-md)
+(require 'ox-html)
 
 (setq org-publish-project-alist
       '(("org"
 	 :base-directory "."
 	 :publishing-directory "publish"
-	 :publishing-function org-md-export-to-markdown
+	 :publishing-function org-html-publish-to-html
 	 :section-numbers t
-	 :with-toc t)))
+	 :with-toc t
+	 :html-head "<link rel=\"stylesheet\" href=\"https://cdn.simplecss.org/simple.css\">")))
