609134c6c2f4873c191ce0127740b043c8e30578
Michal-Atlas <31102317+Michal-Atlas@users.noreply.github.com>
2022-02-12 22:37:47 +0100

    CI: returned to html export

--- publishing.el
+++ publishing.el
@@ -1,9 +1,10 @@
 (require 'org)
+(require 'ox-html)
 
 (setq org-publish-project-alist
       '(("org"
 	 :base-directory "."
 	 :base-extension "org"
 	 :publishing-directory "publish"
-	 :publishing-function org-gfm-export-to-markdown
+	 :publishing-function org-html-export-to-html
 	 :with-toc t)))
